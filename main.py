25269c3d2fcedda9ee7f0fe2c5a1fcfb519304a6
Paolo <p.degrazia@gmail.com>
2016-12-11 20:00:40 +0000

    changed player to omxplayer; created class for musicbutton

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import os
+import time
+from gpiozero import Button
 
 
-os.system('mplayer ./jingle_bells.mp3')
+class MusicButton(Button):
+    def __init__(self, pin, file_path):
+        super(MusicButton, self).__init__(pin)
+        self.file_path = file_path
+        self.is_playing = False
+
+    def play(self):
+        self.is_playing = True
+        os.system('omxplayer ./%s' % self.file_path)
+
+    def stop(self):
+        os.system('killall omxplayer')
+
+
+if __name__ == '__main__':
+    button = MusicButton(2)
+
+    while True:
+        if button.is_pressed:
+            button.play()
+        time.sleep(0.1)
